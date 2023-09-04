6db283a1a20d7802751382c5728d3df7211e1b1c
Kamil <kkarpiak94@icloud.com>
2023-09-04 16:52:33 +0200

    Initial Commit

---
+++ KK_GymApp/KK_GymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KK_GymAppApp.swift
+//  KK_GymApp
+//
+//  Created by Kamil Karpiak on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct KK_GymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

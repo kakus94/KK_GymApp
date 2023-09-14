d1696b9d41807da9bf80d75599a8884fe60d8a64
kamil <kkarpiak94@gmail.com>
2023-09-14 14:58:10 +0200

    end day

--- KK_GymApp/View/CreateTrening.swift
+++ KK_GymApp/View/CreateTrening.swift
@@ -11,14 +11,15 @@ import RealmSwift
 struct CreateTrening: View {
   
   @State var trening: TrainingPlan = .init()
-  @ObservedResults(Exercise.self) var exercises
+//  @ObservedResults(Exercise.self) var exercises
+  
+  @State var sheetShow: Bool = true
   
     var body: some View {
-      List { 
-        ForEach(exercises, id: \.id) { exercise in
-          Text(exercise.name)
+Text("Sa")
+        .sheet(isPresented: $sheetShow) { 
+          ListExercise()
         }
-      }
     }
 }
 
@@ -27,4 +28,30 @@ struct CreateTrening_Previews: PreviewProvider {
         CreateTrening()
         .environment(\.realm, MockRealms.mock())
     }
+}
+
+
+struct ListExercise: View { 
+  
+  @ObservedResults(Exercise.self) var exercises
+  
+  var body: some View { 
+    NavigationStack { 
+      List { 
+        ForEach(exercises, id: \.id) { exercise in
+          HStack {
+            Image(systemName: "photo")
+              .resizable()
+              .frame(width: 45, height: 45)
+            VStack {
+              Text(exercise.name)
+              Text(exercise.category)
+                .font(.footnote)
+            }
+          }
+        }
+      }
+    }
+  }
+  
 }
